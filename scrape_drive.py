6ae58c8fcf5f9d802481088ff71c95df624e3696
Robbie Earle <rb504035@dal.ca>
2018-07-31 09:38:47 -0300

    Ready for code review
    Updated logging

--- scrape_drive.py
+++ scrape_drive.py
@@ -342,20 +342,20 @@ def copy_files(device_id):
             if device_id in active_devices:
 
                 # Mounts device
-                os.system('sudo ~/al_ui/bash_scripts/mount_block.sh ' + device_id +
+                os.system('sudo ~/al_scrape/bash_scripts/mount_block.sh ' + device_id +
                           ' /tmp/temp_device')
 
                 # Makes new directory for this partition
                 os.system('mkdir -p tmp/imported_files' + device_id)
 
                 # Copies files directly into directory to be ingested
-                os.system('cp -a /tmp/temp_device' + ' /tmp/imported_files' + device_id)
+                os.system('cp -a /tmp/temp_device /tmp/imported_files' + device_id)
 
                 # Removes Image
-                os.system('sudo ~/al_ui/bash_scripts/remove_dev_img.sh')
+                os.system('sudo ~/al_scrape/bash_scripts/remove_dev_img.sh')
 
                 # Unmounts device
-                os.system('sudo ~/al_ui/bash_scripts/unmount_block.sh /tmp/temp_device')
+                os.system('sudo ~/al_scrape/bash_scripts/unmount_block.sh /tmp/temp_device')
 
             time.sleep(3)
             # This partition is now finished; subtracts 1 from the partitions that need to be read and returns
