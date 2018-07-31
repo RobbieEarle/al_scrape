f1afb5b2feef93d0cba6243dfac20556c7f32971
Robbie Earle <rb504035@dal.ca>
2018-07-31 11:20:23 -0300

    Getting linux to work again

--- scrape_drive.py
+++ scrape_drive.py
@@ -14,10 +14,32 @@ import time
 
 import logging
 
-from helper.loggers import StreamToLogger, OutputHandler
-
 
 # ============== Logging ==============
+
+class OutputHandler(logging.Handler):
+
+    def __init__(self, socket, *args, **kwargs):
+        logging.Handler.__init__(self, *args, **kwargs)
+        self.socketio = socket
+
+    def emit(self, record):
+        self.socketio.emit('logging', self.format(record))
+
+
+class StreamToLogger(object):
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+    """
+
+    def __init__(self, logger, log_level):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
 
 
 # format_str = '%(asctime)s: %(levelname)s:\t %(name)s: %(message)s'
